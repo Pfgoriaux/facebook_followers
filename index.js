9efd6c5304bbf74722d9507f21852a88ac586124
Paul-François GORIAUX <115989682+Pfgoriaux@users.noreply.github.com>
2025-05-04 14:52:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ app.get('/scrape', async (req, res) => {
   try {
     logger.info(`Starting scrape for: ${query}`, { requestId });
     
-    const { html } = await fetchWithExponentialBackoff(query, {
+    const html = await fetchWithExponentialBackoff(query, {
       method: 'GET',
       headers: {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
